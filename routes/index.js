d844577ae78ee7391e7dacd8650ace6a6218fe9b
MartinOlmedo <martinolmedo985@gmail.com>
2015-06-27 22:27:31 +0200

    cambios en rama creditos

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+router.get('/author', function(req, res, next) {
+	var fullUrl = req.protocol + '://' + req.get('host') ; 
+	console.log("URL:   " + fullUrl);
+  	res.render('author', { title: 'QUIZ', rutaVideo: ( fullUrl+'/demo.mp4'),
+  						rutaFoto: (fullUrl + '/fotoPerfil.png') });
+});	
 
 module.exports = router;
